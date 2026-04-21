5490254cf8336c0f3d0d2abcd250511c825e80f1
Odai Bishr <odaibishr@gmail.com>
2026-04-21 23:49:59 +0300

    feat: add HeaderSection component for displaying title and description

---
+++ src/features/auth/components/HeaderSection.tsx
@@ -0,0 +1,18 @@
+
+type headerSectionProps = {
+    title: string;
+    description: string;
+}
+
+export default function HeaderSection({ title, description }: headerSectionProps) {
+    return (
+        <div className='flex flex-col gap-2 items-center justify-center mb-10'>
+            <h2 className='text-2xl font-semibold'>
+                {title}
+            </h2>
+            <p className='text-sm text-slate-medium'>
+                {description}
+            </p>
+        </div>
+    )
+}
