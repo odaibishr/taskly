69b7c0233fa977a50ba600f5c005ee879f184fe9
Odai Bishr <odaibishr@gmail.com>
2026-04-21 23:15:44 +0300

    feat(navbar): update Navbar component layout and styling

--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -1,8 +1,10 @@
+import Logo from "../../assets/Logo.svg";
+
 export default function Navbar() {
     return (
-        <nav className="w-full bg-background border-b border-border mb-8">
-            <div className="max-w-7xl mx-auto px-4 flex items-center h-12">
-                <img src="/Logo.svg" alt="Taskly Logo" className="h-6 mr-2" />
+        <nav className="w-full bg-background mb-15 px-10 fixed">
+            <div className="max-w-7xl mx-auto px-4 flex justify-between items-center h-20">
+                <img src={Logo} alt="Taskly Logo" className="h-7 mr-2" />
             </div>
         </nav>
     )
