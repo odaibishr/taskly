b4fa293682deb270ff2b2f792b46d347928eb496
Odai Bishr <odaibishr@gmail.com>
2026-05-02 23:52:13 +0300

    feat(auth): implement handleSignIn in auth store

--- src/features/auth/store/auth.store.ts
+++ src/features/auth/store/auth.store.ts
@@ -1,17 +1,20 @@
 import { create } from "zustand";
-import { signUp } from "../api/auth.api";
-import type { SignUpPayload } from "../types";
+import { signIn, signUp } from "../api/auth.api";
+import type { AuthError, LoginPayload, SignUpPayload } from "../types";
 
 interface AuthState {
+	user: any | null;
 	isLoading: boolean;
 	error: string | null;
 	isSignUpSuccess: boolean;
 	handleSignUp: (payload: SignUpPayload) => Promise<void>;
+	handleSignIn: (payload: LoginPayload) => Promise<void>;
 	clearError: () => void;
 	reset: () => void;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
+	user: JSON.parse(localStorage.getItem('user') || 'null'),
 	isLoading: false,
 	error: null,
 	isSignUpSuccess: false,
@@ -22,6 +25,25 @@ export const useAuthStore = create<AuthState>((set) => ({
 			set({ isSignUpSuccess: true });
 		} catch (error: any) {
 			const message = error.response?.data?.msg || error.message || 'Something went wrong. Please try again.';
+			set({ error: message });
+		} finally {
+			set({ isLoading: false });
+		}
+	},
+	handleSignIn: async (payload) => {
+		set({
+			isLoading: true,
+			error: null,
+		});
+		try {
+			const data = await signIn(payload);
+			localStorage.setItem('access_token', data.access_token);
+			localStorage.setItem('refresh_token', data.refresh_token);
+			localStorage.setItem('user', JSON.stringify(data.user));
+			set({ user: data.user });
+			window.location.href = '/'
+		} catch (error: any) {
+			const message = error.response?.data?.error_description || error.message || 'Login failed.';
 			set({ error: message });
 		} finally {
 			set({ isLoading: false });
