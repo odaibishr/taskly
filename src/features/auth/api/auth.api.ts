0607bbd6a687e1f04684468672b7e4deba83f50f
Odai Bishr <odaibishr@gmail.com>
2026-05-02 23:39:47 +0300

    feat(auth): add signIn api method

--- src/features/auth/api/auth.api.ts
+++ src/features/auth/api/auth.api.ts
@@ -1,7 +1,12 @@
 import { http } from "../../../shared/lib/http";
-import type { SignUpPayload } from "../types";
+import type { LoginPayload, SignUpPayload } from "../types";
 
 export async function signUp(payload: SignUpPayload) {
 	const response = await http.post('/auth/v1/signup', payload);
+	return response.data;
+}
+
+export async function signIn(payload: LoginPayload) {
+	const response = await http.post('/auth/v1/token?grant_type=password', payload);
 	return response.data;
 }
