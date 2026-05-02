d350f9b00388931cf9d128ac619b86c98922c37d
Odai Bishr <odaibishr@gmail.com>
2026-05-03 00:06:34 +0300

    feat(auth): add LogInPage

---
+++ src/pages/LogInPage.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "../features/auth/components/LoginForm";
+
+export default function LogInPage() {
+	return (
+		<div className="max-w-md mx-auto p-4">
+			<LoginForm />
+		</div>
+	)
+}
