9dda5fc5deccdcffa4bc5646f0f6109349883fbd
Odai Bishr <odaibishr@gmail.com>
2026-05-03 00:07:07 +0300

    feat: add LogInPage to router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 
 import SignUpPage from "../pages/SignUpPage";
+import LogInPage from "../pages/LogInPage";
 
 const router = createBrowserRouter([
 	{
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
 		path: "/signup",
 		element: <SignUpPage />,
 	},
+	{
+		path: "/login",
+		element: <LogInPage />,
+	}
 ]);
 
 export function AppRouter() {
