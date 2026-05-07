b83457585e6fbdbb48547e585d8af24c44f102bb
Odai Bishr <odaibishr@gmail.com>
2026-05-08 00:30:01 +0300

    feat; add frogot password form

---
+++ src/features/auth/components/ForgotPasswodForm.tsx
@@ -0,0 +1,70 @@
+import type z from "zod";
+import { forgotPasswordSchema } from "../validation";
+import { useAuthStore } from "../store/auth.store";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+import FormContainer from "../../../shared/components/FormContainer";
+import HeaderSection from "./HeaderSection";
+import Input from "./Input";
+import Button from "../../../shared/components/Button";
+import { Link } from "react-router-dom";
+import ArrowLeftIcon from '../../../assets/ArrowLeft.svg';
+
+
+
+type ForgotPasswordFormData = z.infer<typeof forgotPasswordSchema>;
+
+export default function ForgotPasswordForm() {
+	const [timer, setTimer] = useState<number>(0);
+	const { isLoading, error, handleForgotPassword, clearError } = useAuthStore();
+	const { register, handleSubmit, formState: { errors } } = useForm<ForgotPasswordFormData>({
+		resolver: zodResolver(forgotPasswordSchema),
+	});
+
+	const onSubimt = async (data: ForgotPasswordFormData) => {
+		await handleForgotPassword(data);
+	}
+
+	useEffect(() => {
+		let intervalId: ReturnType<typeof setInterval>;
+		if (timer > 0) {
+			intervalId = setInterval(() => {
+				setTimer((prev) => prev - 1);
+			}, 1000);
+		}
+
+		return () => clearInterval(intervalId);
+	}, [timer]);
+
+	return (
+		<FormContainer>
+			<HeaderSection
+				title="Forgot password?"
+				description="No worries, we'll send you reset instructions."
+			/>
+
+			<form onSubmit={handleSubmit(onSubimt)}>
+				<Input
+					register={register}
+					name="email"
+					placeholder="Email"
+					label="Email"
+					error={errors.email}
+				/>
+
+				<Button
+					type="submit"
+					disabled={isLoading || timer > 0}
+				>
+					Send Reset Link
+				</Button>
+
+				<Link to="/login" className="text-blue-500 text-sm flex justify-center mt-10 gap-1">
+					<img src={ArrowLeftIcon} alt="Back to login" />
+					<span>Back to login</span>
+				</Link>
+			</form>
+		</FormContainer>
+	);
+}
