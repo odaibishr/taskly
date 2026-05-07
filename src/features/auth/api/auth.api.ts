f5acf0b05a2fde2448fb90fd0a0a3b606275fb66
Odai Bishr <odaibishr@gmail.com>
2026-05-07 23:38:23 +0300

    feat: add SendResetLink method to auth.api.ts

--- src/features/auth/api/auth.api.ts
+++ src/features/auth/api/auth.api.ts
@@ -1,5 +1,5 @@
 import { http } from "../../../shared/lib/http";
-import type { LoginPayload, SignUpPayload } from "../types";
+import type { LoginPayload, SendResetLinkPayload, SignUpPayload } from "../types";
 
 export async function signUp(payload: SignUpPayload) {
 	const response = await http.post('/auth/v1/signup', payload);
@@ -8,5 +8,10 @@ export async function signUp(payload: SignUpPayload) {
 
 export async function signIn(payload: LoginPayload) {
 	const response = await http.post('/auth/v1/token?grant_type=password', payload);
+	return response.data;
+}
+
+export async function sendResetLink(payload: SendResetLinkPayload) {
+	const response = await http.post('/auth/v1/recover', payload);
 	return response.data;
 }
