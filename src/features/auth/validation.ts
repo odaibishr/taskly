c488d16433615ca6f22a5972c506da74a2554b78
Odai Bishr <odaibishr@gmail.com>
2026-05-08 00:07:30 +0300

    feat: add forgotPasswordSchema to validation

--- src/features/auth/validation.ts
+++ src/features/auth/validation.ts
@@ -18,3 +18,7 @@ export const loginSchema = z.object({
     email: z.string().min(1, "Email is required").email("Invalid email format"),
     password: z.string().min(1, "Password is required"),
 });
+
+export const forgotPasswordSchema = z.object({
+    email: z.string().min(1, 'Email is required').email('Invalid email format'),
+});
