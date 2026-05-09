1bada35851a4df3f585c99bbdb3a47140f49cd39
Odai Bishr <odaibishr@gmail.com>
2026-05-09 22:39:04 +0300

    feat: add getInitails to get The user's avatar letters

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function getInitials(name: string): string {
+    if (!name) return '';
+    const parts = name.trim().split(/\s+/);
+    if (parts.length > 1)
+        return (parts[0][0] + parts[1][0]).toUpperCase();
+    return name.substring(0, 2).toUpperCase();
+}
