fad9d084f1fabcaf0d137ff29dee10b1a6df5f32
Odai Bishr <odaibishr@gmail.com>
2026-05-11 23:52:55 +0300

    delete the an useage tags

--- src/pages/CreateProjectPage.tsx
+++ src/pages/CreateProjectPage.tsx
@@ -1,14 +1,10 @@
-import Button from "../shared/components/Button"
 import { HeaderSection } from "../shared/components/HeaderSection"
 
 const CreateProjectPage = () => {
   return (
     <main>
       <HeaderSection />
-      <div className="flex justify-between items-center mb-6">
-        <h1 className="text-2xl font-bold">Create Project</h1>
-        <Button>Create Project</Button>
-      </div>
+
     </main>
   )
 }
