7a656b2dfe68c60cd7243481d3f863fd2e148670
Odai Bishr <odaibishr@gmail.com>
2026-05-12 00:45:13 +0300

    feat: add Create Project Logic

--- src/features/projects/components/CreateProjectForm.tsx
+++ src/features/projects/components/CreateProjectForm.tsx
@@ -1,5 +1,37 @@
+import z from "zod";
+import { createProjectSchema } from "../validation"
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useProjecteStore } from "../store/projects.store";
+
+type CreateProjectFormData = z.infer<typeof createProjectSchema>;
 
 const CreateProjectForm = () => {
+	const navigate = useNavigate();
+	const {
+		createProject,
+		isLoading,
+		error
+	} = useProjecteStore();
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm<CreateProjectFormData>({
+		resolver: zodResolver(createProjectSchema),
+	});
+
+	const onSubmit = async (data: CreateProjectFormData) => {
+		try {
+			await createProject(data);
+			navigate('/dashboard/projects');
+		} catch (error: any) {
+			console.error(error.message)
+		}
+	};
+
 	return (
 		<div>CreateProjectForm</div>
 	)
