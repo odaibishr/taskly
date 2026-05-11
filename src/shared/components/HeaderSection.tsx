d70bdd7a8e73d1dcebe73f88eb35458445132698
Odai Bishr <odaibishr@gmail.com>
2026-05-11 23:42:10 +0300

    feat: add breadcrumb to headersection

--- src/shared/components/HeaderSection.tsx
+++ src/shared/components/HeaderSection.tsx
@@ -1,6 +1,19 @@
+import Breadcrumbs from "./Breadcrumbs";
+import { useLocation } from "react-router-dom";
 
 export const HeaderSection = () => {
+	const location = useLocation();
+	const pathnames = location.pathname.split("/").filter((x) => x);
+	const currentPath = pathnames[pathnames.length - 1] || "Dashboard";
+
+	const pageTitle = currentPath.replace(/-/g, " ");
+
 	return (
-		<div>HeaderSection</div>
-	)
-}
+		<section className="flex flex-col gap-2 mb-8">
+			<Breadcrumbs />
+			<h1 className="text-3xl font-bold capitalize">
+				{pageTitle}
+			</h1>
+		</section>
+	);
+};
