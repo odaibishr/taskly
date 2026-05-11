14a7b518d302601a82abc3cb841f0272b1c18192
Odai Bishr <odaibishr@gmail.com>
2026-05-11 23:57:27 +0300

    update: add title and desc to HeaderSection

--- src/shared/components/HeaderSection.tsx
+++ src/shared/components/HeaderSection.tsx
@@ -1,7 +1,13 @@
 import Breadcrumbs from "./Breadcrumbs";
 import { useLocation } from "react-router-dom";
 
-export const HeaderSection = ({ children }: { children?: React.ReactNode }) => {
+interface Props {
+	title?: string;
+	description?: string;
+	children?: React.ReactNode;
+}
+
+export const HeaderSection = ({ title, description, children }: Props) => {
 	const location = useLocation();
 	const pathnames = location.pathname.split("/").filter((x) => x);
 	const currentPath = pathnames[pathnames.length - 1] || "Dashboard";
@@ -12,9 +18,12 @@ export const HeaderSection = ({ children }: { children?: React.ReactNode }) => {
 		<section className="flex flex-col gap-2 mb-8">
 			<Breadcrumbs />
 			<div className="flex justify-between items-center gap-5">
-				<h1 className="text-3xl font-bold capitalize">
-					{pageTitle}
-				</h1>
+				<div className="flex flex-col gap-1">
+					<h1 className="text-3xl font-bold capitalize">
+						{title ? title : pageTitle}
+					</h1>
+					{description && <p className="text-gray-500">{description}</p>}
+				</div>
 				{children}
 			</div>
 		</section>
