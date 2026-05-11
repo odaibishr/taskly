dca51c42229ba346d52626f334b6f58a02b0ac9a
Odai Bishr <odaibishr@gmail.com>
2026-05-11 23:02:17 +0300

    feat: add an Empty project componetn

--- src/features/projects/components/EmptyProjects.tsx
+++ src/features/projects/components/EmptyProjects.tsx
@@ -1,7 +1,23 @@
+import Button from "../../../shared/components/Button"
+import NoProjects from "../../../assets/Abstract.svg"
+import { PlusCircle } from "lucide-react"
 
 const EmptyProjects = () => {
 	return (
-		<div>EmptyProjects</div>
+		<section className="flex flex-col items-center justify-center mt-20 gap-11">
+			<img src={NoProjects} alt="No Projects" className="w-50 h-50" />
+			<div className="flex flex-col gap-4 justify-center items-center text-center">
+				<h2 className="text-2xl font-bold my-2">No Projects</h2>
+				<p className="text-slate-dark">You don’t have any projects yet. Start by defining
+					your first architectural workspace to begin tracking tasks and epics.</p>
+			</div>
+			<Button
+				className="cursor-pointer flex gap-3 items-center w-45 shadow-md"
+			>
+				<PlusCircle className="w-5 h-5" />
+				<span>Create Project</span>
+			</Button>
+		</section>
 	)
 }
 
