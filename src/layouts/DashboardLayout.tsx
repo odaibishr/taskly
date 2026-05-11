37c0b26cef868d0b2949725bb923ed512c08dded
Odai Bishr <odaibishr@gmail.com>
2026-05-11 21:39:11 +0300

    feat: protect routes

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -2,9 +2,17 @@ import { useState } from "react";
 import Navbar from "../shared/components/Navbar";
 import Sidebar from "../shared/components/Sidebar";
 import { cn } from "../shared/lib/utils";
+import { useAuthStore } from "../features/auth/store/auth.store";
+import { Navigate } from "react-router-dom";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
 	const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
+	const { user } = useAuthStore();
+
+	if (!user) {
+		return <Navigate to="/login" replace />;
+	}
+
 
 	return (
 		<div className="min-h-screen flex">
