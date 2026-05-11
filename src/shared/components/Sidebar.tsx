c40b7c7ae0fe9dca12c68ee529f7e5c174d46425
Odai Bishr <odaibishr@gmail.com>
2026-05-11 19:23:03 +0300

    feat: add mobile menu button

--- src/shared/components/Sidebar.tsx
+++ src/shared/components/Sidebar.tsx
@@ -6,6 +6,7 @@ import Tasks from '../../assets/Tasks.svg';
 import Users from '../../assets/Users.svg';
 import Logout from '../../assets/Logout.svg';
 import Info from '../../assets/Info.svg';
+import Menu from '../../assets/MenuIcon.svg';
 import type { MenuItem } from "../types/types";
 import { useAuthStore } from "../../features/auth/store/auth.store";
 import { useNavigate } from "react-router-dom";
@@ -17,14 +18,14 @@ const menuItems: MenuItem[] = [
 		icon: Folder,
 		label: 'Projects',
 		herf: '/dashboard/projects',
-	},	
+	},
 	{
 		id: 2,
 		icon: Epics,
 		label: 'Project Epics',
 		herf: '/dashboard/project-epics',
 	},
-	
+
 	{
 		id: 3,
 		icon: Tasks,
@@ -56,5 +57,16 @@ export default function Sidebar() {
 		navigate('/login');
 	}
 
-	
+	return (
+		<>
+			<button
+				className="lg:hidden fixed top-5 left-5 z-50 p-2 bg-white rounded-md shadow-md"
+				onClick={() => setIsMobileOpen(!isMobileOpen)}
+			>
+				{!isMobileOpen && <img src={Menu} style={{ width: '20px', height: '20px' }} />}
+			</button>
+		</>
+	);
+
+
 }
