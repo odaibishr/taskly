e68318eed82837da164b1d14ff3e97c56aa6f7db
Odai Bishr <odaibishr@gmail.com>
2026-05-11 19:05:06 +0300

    feat: add sidebar assets and create the  Sidbar compnent

---
+++ src/shared/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import ArrawLeft from '../../assets/ArrawLeft.svg';
+import Epics from '../../assets/Epics.svg';
+import Folder from '../../assets/Folder.svg';
+import Tasks from '../../assets/Tasks.svg';
+import Users from '../../assets/Users.svg';
+import Logout from '../../assets/Logout.svg';
+import Info from '../../assets/Info.svg';
+
+
+export default function Sidebar() {
+	
+}
