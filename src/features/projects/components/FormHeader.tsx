1c71c87e3dd91768a582ed36c369b21371c359a9
Odai Bishr <odaibishr@gmail.com>
2026-05-12 12:29:53 +0300

    feat(projects): update form header styling and content

--- src/features/projects/components/FormHeader.tsx
+++ src/features/projects/components/FormHeader.tsx
@@ -1,7 +1,25 @@
+import type React from "react"
 
-const FormHeader = () => {
+type FormHeaderProps = {
+	title: string,
+	description: string,
+	icon: React.ReactNode,
+}
+
+const FormHeader = ({ title, description, icon }: FormHeaderProps) => {
 	return (
-		<div>FormHeader</div>
+		<div className="h-[125px] px-8 pt-8 pb-10 border-b border-surface-low">
+			<div className="flex items-center gap-4">
+				<div className="flex items-center justify-center p-3 h-11 w-11 bg-primary-container/10 rounded-sm">
+					{icon}
+				</div>
+				<div className="flex flex-col gap-0.5">
+					<h1 className="text-2xl font-bold text-slate-dark">{title}</h1>
+					<p className="text-body-md text-slate-medium leading-5">{description}</p>
+				</div>
+			</div>
+
+		</div>
 	)
 }
 
