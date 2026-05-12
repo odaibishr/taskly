07355aaf0a7dceb3364a56ff083ba7bf2eb61226
Odai Bishr <odaibishr@gmail.com>
2026-05-12 11:58:37 +0300

    feat: add CreateProjectForm createproject page

--- src/pages/CreateProjectPage.tsx
+++ src/pages/CreateProjectPage.tsx
@@ -1,10 +1,11 @@
+import CreateProjectForm from "../features/projects/components/CreateProjectForm"
 import { HeaderSection } from "../shared/components/HeaderSection"
 
 const CreateProjectPage = () => {
   return (
     <main>
       <HeaderSection title="Create Project" />
-
+      <CreateProjectForm />
     </main>
   )
 }
