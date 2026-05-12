0b5cd4a15ca6899cf28025201c4d31664786930d
Odai Bishr <odaibishr@gmail.com>
2026-05-12 12:54:17 +0300

    feat(projects): implement create project form UI and validation logic

--- src/features/projects/components/CreateProjectForm.tsx
+++ src/features/projects/components/CreateProjectForm.tsx
@@ -4,6 +4,11 @@ import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useProjecteStore } from "../store/projects.store";
+import FormHeader from "./FormHeader";
+import { CheckCircle } from "lucide-react";
+import Input from "../../../shared/components/Input";
+import Textarea from "../../../shared/components/Texterea";
+import Button from "../../../shared/components/Button";
 
 type CreateProjectFormData = z.infer<typeof createProjectSchema>;
 
@@ -11,7 +16,6 @@ const CreateProjectForm = () => {
 	const navigate = useNavigate();
 	const {
 		createProject,
-		isLoading,
 		error
 	} = useProjecteStore();
 
@@ -28,12 +32,62 @@ const CreateProjectForm = () => {
 			await createProject(data);
 			navigate('/dashboard/projects');
 		} catch (error: any) {
-			console.error(error.message)
+			console.error(error.message);
 		}
 	};
 
 	return (
-		<div>CreateProjectForm</div>
+		<section className="flex items-center justify-center mt-16">
+			<div className="bg-white w-[80%] rounded-lg shadow-[0px_24px_48px_0px_#041B3C0F]">
+				<FormHeader
+					title="Initialize New Project"
+					description="Define the scope and foundational details of your project."
+					icon={<CheckCircle className="text-primary-container" color="#003D9B" size={32} />}
+				/>
+
+				<form onSubmit={handleSubmit(onSubmit)} className="p-8">
+					<Input
+						name="name"
+						placeholder="Enter project title"
+						register={register}
+						error={errors.name}
+						label="Project Name"
+					/>
+					<br />
+					<Textarea
+						name="description"
+						placeholder="Enter project description"
+						register={register}
+						error={errors.description}
+						label="Project Description"
+						maxLength={500}
+						optional
+					/>
+
+					<div className="pt-6 flex items-center justify-between gap-4">
+						<Button
+							type="button"
+							variant="ghost"
+							onClick={() => navigate('/dashboard/projects')}
+							className="px-8"
+						>
+							Back
+						</Button>
+
+						<Button
+							type="submit"
+							variant="primary"
+							className="min-w-[160px]"
+						>
+							Create Project
+						</Button>
+					</div>
+
+				</form>
+			</div>
+
+
+		</section>
 	)
 }
 
