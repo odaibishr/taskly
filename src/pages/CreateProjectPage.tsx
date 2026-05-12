56d539ee24a7b4798d1a625fb8dcbfc2b9194755
Odai Bishr <odaibishr@gmail.com>
2026-05-13 01:21:58 +0300

    feat: add invite member button to create project page

--- src/pages/CreateProjectPage.tsx
+++ src/pages/CreateProjectPage.tsx
@@ -1,10 +1,20 @@
+import { UserPlus } from "lucide-react"
 import CreateProjectForm from "../features/projects/components/CreateProjectForm"
+import Button from "../shared/components/Button"
 import { HeaderSection } from "../shared/components/HeaderSection"
 
 const CreateProjectPage = () => {
   return (
     <main>
-      <HeaderSection title="Create Project" />
+      <HeaderSection title="Create Project">
+        <Button
+          variant="primary"
+          className="max-md:hidden cursor-pointer flex gap-3 items-center shadow-md"
+        >
+          <UserPlus className="w-5 h-5" />
+          <span>Invite Member</span>
+        </Button>
+      </HeaderSection>
       <CreateProjectForm />
     </main>
   )
