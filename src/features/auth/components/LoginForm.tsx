130641aef7af97712d2166b1502ef56182e3a748
Odai Bishr <odaibishr@gmail.com>
2026-05-12 12:37:29 +0300

    chore: change the path of the Input component

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -5,7 +5,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import FormContainer from "../../../shared/components/FormContainer";
 import HeaderSection from "./HeaderSection";
-import Input from "./Input";
+import Input from "../../../shared/components/Input";
 import Button from "../../../shared/components/Button";
 import { Link } from "react-router-dom";
 
