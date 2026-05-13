9a8a60fa554100843b3934d3fef95770c5b73c57
Odai Bishr <odaibishr@gmail.com>
2026-05-13 23:04:43 +0300

    feat: add getProjectsParams and return the data with the total count

--- src/features/projects/api/projects.api.ts
+++ src/features/projects/api/projects.api.ts
@@ -1,12 +1,25 @@
 import { http } from "../../../shared/lib/http";
-import type { CreateProjectPayload } from "../types";
+import type { CreateProjectPayload, GetProjectsParams } from "../types";
 
 export async function createProject(payload: CreateProjectPayload) {
 	const response = await http.post('/rest/v1/projects', payload);
 	return response.data[0];
 }
 
-export async function getProjects() {
-	const response = await http.get('/rest/v1/rpc/get_projects');
-	return response.data;
+export async function getProjects(params: GetProjectsParams) {
+	const response = await http.get(
+		'/rest/v1/rpc/get_projects',
+		{
+			params,
+			headers: {
+				'Prefer': 'count=exact'
+			}
+		});
+
+	const contentRange = response.headers['content-range'];
+	const totalCount = contentRange ? parseInt(contentRange.split('/')[1]) : 0;
+	return {
+		data: response.data,
+		totalCount
+	}
 }
