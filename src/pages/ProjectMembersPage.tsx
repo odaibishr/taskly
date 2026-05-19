e39615e700e6d170df77272d0804de55da58573e
Odai Bishr <odaibishr@gmail.com>
2026-05-20 01:03:35 +0300

    feat: implement header section

--- src/pages/ProjectMembersPage.tsx
+++ src/pages/ProjectMembersPage.tsx
@@ -1,6 +1,20 @@
+import { UserPlusIcon } from "lucide-react"
+import Button from "../shared/components/Button"
+import { HeaderSection } from "../shared/components/HeaderSection"
+
 const ProjectMembersPage = () => {
     return (
-        <div>ProjectMembersPage</div>
+        <main>
+            <HeaderSection title="Project Members" isBreadcrumbVisible={true} >
+                <Button
+                    variant="primary"
+                    className="flex items-center gap-2"
+                >
+                    <UserPlusIcon size={20} />
+                    Invite Members
+                </Button>
+            </HeaderSection>
+        </main>
     )
 }
 
