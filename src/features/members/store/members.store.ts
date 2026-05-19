749ca82fedab730f2c57b9c7b9195fc680883251
Odai Bishr <odaibishr@gmail.com>
2026-05-20 00:25:06 +0300

    feat: create members store

--- src/features/members/store/members.store.ts
+++ src/features/members/store/members.store.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import type { ProjectMember } from "../types";
 import { getProjectMembers } from "../api/members.api";
+import { fa } from "zod/locales";
 
 interface MembersState {
     members: ProjectMember[];
@@ -8,3 +9,12 @@ interface MembersState {
     isLoading: boolean;
     clearError: () => void;
 }
+
+export const useMembersStore = create<MembersState>()((set) => ({
+    members: [],
+    isLoading: false,
+    error: null,
+    clearError: () => set({ error: null }),
+
+    //actions
+}))
