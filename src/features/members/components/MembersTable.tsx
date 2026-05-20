0e29688b87acb32167677dd95abe0f343028dd3b
Odai Bishr <odaibishr@gmail.com>
2026-05-20 20:44:14 +0300

    feat: add no members found message to MembersTable

--- src/features/members/components/MembersTable.tsx
+++ src/features/members/components/MembersTable.tsx
@@ -1,3 +1,4 @@
+import { Users } from "lucide-react"
 import type { ProjectMember } from "../types"
 
 interface MembersTableProps {
@@ -6,6 +7,18 @@ interface MembersTableProps {
 
 
 const MembersTable = ({ members }: MembersTableProps) => {
+
+    if (members.length === 0) {
+        return (
+            <div
+                className="w-full bg-white rounded-xl border border-gray-100 shadow-xs p-12 text-center flex flex-col items-center justify-center"
+            >
+                <Users
+                    size={48} className="text-slate-medium mb-4" />
+                <p className="text-slate-medium font-medium">No members found in this project.</p>
+            </div>
+        )
+    }
     return (
         <div>MembersTable</div>
     )
