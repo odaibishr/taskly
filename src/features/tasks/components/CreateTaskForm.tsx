280c980e4d869236c933e323160cf1a4965f5e8f
Odai Bishr <odaibishr@gmail.com>
2026-05-31 22:42:01 +0300

    feat: add CreateTaskFormData type and statusOptions

--- src/features/tasks/components/CreateTaskForm.tsx
+++ src/features/tasks/components/CreateTaskForm.tsx
@@ -1,3 +1,19 @@
+import type z from "zod";
+import type { createTaskSchema } from "@/features/tasks/validation";
+
+type CreateTaskFormData = z.infer<typeof createTaskSchema>;
+
+const statusOptions = [
+    { value: "TO_DO", label: "TO DO" },
+    { value: "IN_PROGRESS", label: "IN PROGRESS" },
+    { value: "BLOCKED", label: "BLOCKED" },
+    { value: "IN_REVIEW", label: "IN REVIEW" },
+    { value: "READY_FOR_QA", label: "READY FOR QA" },
+    { value: "REOPENED", label: "REOPENED" },
+    { value: "READY_FOR_PRODUCTION", label: "READY FOR PRODUCTION" },
+    { value: "DONE", label: "DONE" },
+];
+
 const CreateTaskForm = () => {
     return <div>CreateTaskForm</div>;
 };
