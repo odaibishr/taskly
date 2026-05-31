e7cf6c36611759ea81698787ab66d16c3b669b06
Odai Bishr <odaibishr@gmail.com>
2026-05-31 22:35:11 +0300

    feat: implement createTask method in tasksStore

--- src/features/tasks/store/tasks.store.ts
+++ src/features/tasks/store/tasks.store.ts
@@ -1,12 +1,42 @@
 import { create } from "zustand";
-import type { CreateTaskPayload, Task } from "../types";
+import type { CreateTaskPayload, Task } from "@/features/tasks/types";
+import { createTask } from "@/features/tasks/api/tasks.api";
 
 
 
 interface TasksState {
+    tasks: Task[];
     isLoading: boolean;
     error: string | null;
     clearError: () => void;
-    createTask: (payload: CreateTaskPayload) => Promise<Task>;
+    createTask: (payload: CreateTaskPayload) => Promise<void>;
 }
 
+export const useTasksStore = create<TasksState>()((set, get) => ({
+    tasks: [],
+    isLoading: false,
+    error: null,
+    createTask: async (payload: CreateTaskPayload) => {
+        set({
+            isLoading: true,
+            error: null,
+        })
+
+        try {
+            const data = await createTask(payload);
+            set({
+                tasks: [...get().tasks, data],
+            });
+        } catch (error: unknown) {
+            const message = error instanceof Error ? error.message : "Failed to create task";
+            set({
+                error: message,
+            });
+        } finally {
+            set({
+                isLoading: false,
+            });
+        }
+    },
+    clearError: () => set({ error: null }),
+}))
