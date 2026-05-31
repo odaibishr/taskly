3ae5bc959e50f65ca9af1987e2c1ecf08c9cb5f5
Odai Bishr <odaibishr@gmail.com>
2026-06-01 00:59:05 +0300

    feat: add get epics tasks method to store

--- src/features/tasks/store/tasks.store.ts
+++ src/features/tasks/store/tasks.store.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
-import { createTask } from "@/features/tasks/api/tasks.api";
-import type { CreateTaskPayload, Task } from "@/features/tasks/types";
+import { createTask, fetchTasksByEpicId } from "@/features/tasks/api/tasks.api";
+import type { CreateTaskPayload, ProjectTask, Task } from "@/features/tasks/types";
 
 
 
@@ -11,6 +11,11 @@ interface TasksState {
     error: string | null;
     clearError: () => void;
     createTask: (payload: CreateTaskPayload) => Promise<void>;
+    epicTasks: ProjectTask[];
+    isEpicTasksLoading: boolean;
+    epicTasksError: string | null;
+    getEpicTasks: (epicId: string) => Promise<void>;
+    clearEpicTasks: () => void;
 }
 
 export const useTasksStore = create<TasksState>()((set, get) => ({
@@ -33,6 +38,7 @@ export const useTasksStore = create<TasksState>()((set, get) => ({
             set({
                 error: message,
             });
+            throw new Error(message);
         } finally {
             set({
                 isLoading: false,
@@ -40,4 +46,28 @@ export const useTasksStore = create<TasksState>()((set, get) => ({
         }
     },
     clearError: () => set({ error: null }),
+    epicTasks: [],
+    isEpicTasksLoading: false,
+    epicTasksError: null,
+    getEpicTasks: async (epicId: string) => {
+        set({
+            isEpicTasksLoading: true,
+            epicTasksError: null,
+        });
+        try {
+            const data = await fetchTasksByEpicId(epicId);
+            set({
+                epicTasks: data,
+            });
+        } catch {
+            set({
+                epicTasksError: "Failed to load tasks",
+            });
+        } finally {
+            set({
+                isEpicTasksLoading: false,
+            });
+        }
+    },
+    clearEpicTasks: () => set({ epicTasks: [], epicTasksError: null }),
 }))
