5fd6f654a1b0c423abbaff25757f1c4f037fb84d
Odai Bishr <odaibishr@gmail.com>
2026-06-01 01:00:24 +0300

    feat:  export TaskList from index

--- src/features/tasks/index.ts
+++ src/features/tasks/index.ts
@@ -1,4 +1,5 @@
 export { default as CreateTaskForm } from "@/features/tasks/components/CreateTaskForm";
+export { TaskList } from "@/features/tasks/components/TaskList";
 export * from "@/features/tasks/store/tasks.store";
 export * from "@/features/tasks/types";
 export * from "@/features/tasks/validation";
