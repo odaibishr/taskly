d0f11b82036f6d5b8b2733343c4869716c8350cb
Odai Bishr <odaibishr@gmail.com>
2026-05-31 23:25:32 +0300

    feat: add CreateTaskPage to router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter, Navigate, Outlet, RouterProvider } from "react-router-dom";
 
-
 import DashboardLayout from "@/layouts/DashboardLayout";
 import Navbar from "@/layouts/Navbar";
 import CreateEpicPage from "@/pages/CreateEpicPage";
@@ -14,61 +13,63 @@ import ProjectsPage from "@/pages/ProjectsPage";
 import ResetPasswordPage from "@/pages/ResetPasswordPage";
 import SignUpPage from "@/pages/SignUpPage";
 import { useRecoveryRedirect } from "@/shared/hooks/recoveryRedirect";
+import CreateTaskPage from "@/pages/CreateTaskPage";
 
 function RootLayout() {
-	useRecoveryRedirect();
-	return (
-		<>
-			<Navbar />
-			<Outlet />
-		</>
-	);
+    useRecoveryRedirect();
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+        </>
+    );
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <RootLayout />,
-		children: [
-			{ index: true, element: <Navigate to="/login" replace /> },
-			{ path: "/signup", element: <SignUpPage /> },
-			{ path: "/login", element: <LogInPage /> },
-			{ path: "/forget-password", element: <ForgotPasswordPage /> },
-			{ path: "/reset-password", element: <ResetPasswordPage /> },
-		],
-	},
-	{
-		path: "/project",
-		element: (
-			<DashboardLayout>
-				<Outlet />
-			</DashboardLayout>
-		),
+    {
+        path: "/",
+        element: <RootLayout />,
+        children: [
+            { index: true, element: <Navigate to="/login" replace /> },
+            { path: "/signup", element: <SignUpPage /> },
+            { path: "/login", element: <LogInPage /> },
+            { path: "/forget-password", element: <ForgotPasswordPage /> },
+            { path: "/reset-password", element: <ResetPasswordPage /> },
+        ],
+    },
+    {
+        path: "/project",
+        element: (
+            <DashboardLayout>
+                <Outlet />
+            </DashboardLayout>
+        ),
 
-		children: [
-			{
-				index: true,
-				element: <ProjectsPage />,
-			},
-			{
-				path: "create-project",
-				element: <CreateProjectPage />,
-			},
-			{
-				path: ":projectId",
-				children: [
-					{ path: "epics", element: <EpicsPage /> },
-					{ path: "epics/new", element: <CreateEpicPage /> },
-					{ path: "tasks", element: <div>Tasks Page Content</div> },
-					{ path: "members", element: <ProjectMembersPage /> },
-					{ path: "edit", element: <EditProjectPage /> },
-				]
-			}
-		]
-	},
+        children: [
+            {
+                index: true,
+                element: <ProjectsPage />,
+            },
+            {
+                path: "create-project",
+                element: <CreateProjectPage />,
+            },
+            {
+                path: ":projectId",
+                children: [
+                    { path: "epics", element: <EpicsPage /> },
+                    { path: "epics/new", element: <CreateEpicPage /> },
+                    { path: "tasks", element: <div>Tasks Page Content</div> },
+                    { path: "tasks/new", element: <CreateTaskPage /> },
+                    { path: "members", element: <ProjectMembersPage /> },
+                    { path: "edit", element: <EditProjectPage /> },
+                ],
+            },
+        ],
+    },
 ]);
 
 export function AppRouter() {
-	return <RouterProvider router={router} />;
-}
+    return <RouterProvider router={router} />;
+}
