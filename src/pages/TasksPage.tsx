17980b532dd94c32e5575173f8327a037d62dd84
Odai Bishr <odaibishr@gmail.com>
2026-06-02 00:11:30 +0300

    feat: implement TasksPage  to render project-specific task boards

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -1,5 +1,25 @@
+import { TasksBoardView } from "@/features/tasks";
+import ErrorCard from "@/shared/components/ErrorCard";
+import { HeaderSection } from "@/shared/components/HeaderSection";
+import { useParams } from "react-router-dom";
+
 const TasksPage = () => {
-    return <div>TasksPage</div>;
+    const { projectId } = useParams<{ projectId: string }>();
+
+    return (
+        <main className="w-full">
+            <HeaderSection
+                title="Active Workboard"
+                description="Curating Project Alpha's production pipeline and milestones."
+                isBreadcrumbVisible={true}
+            />
+            {projectId ? (
+                <TasksBoardView projectId={projectId} />
+            ) : (
+                <ErrorCard retryAction={() => {}} />
+            )}
+        </main>
+    );
 };
 
 export default TasksPage;
