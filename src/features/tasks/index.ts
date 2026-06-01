1751649cd7a414b1ab7229874fe1b92140c4e11d
Odai Bishr <odaibishr@gmail.com>
2026-06-02 00:11:05 +0300

    feat: export TasksBoardView component from index

--- src/features/tasks/index.ts
+++ src/features/tasks/index.ts
@@ -1,4 +1,5 @@
 export { default as CreateTaskForm } from "@/features/tasks/components/CreateTaskForm";
+export { default as TasksBoardView } from "@/features/tasks/components/TasksBoardView";
 export { TaskList } from "@/features/tasks/components/TaskList";
 export * from "@/features/tasks/store/tasks.store";
 export * from "@/features/tasks/types";
