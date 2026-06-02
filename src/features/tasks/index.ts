423db54d191d385e98d89ea0a12d2a922dc8afe0
Odai Bishr <odaibishr@gmail.com>
2026-06-02 23:51:06 +0300

    feat: export task details modal

--- src/features/tasks/index.ts
+++ src/features/tasks/index.ts
@@ -2,6 +2,7 @@ export { default as CreateTaskForm } from "@/features/tasks/components/CreateTas
 export { default as TasksBoardView } from "@/features/tasks/components/TasksBoardView";
 export { default as TasksListView } from "@/features/tasks/components/TasksListView";
 export { TaskList } from "@/features/tasks/components/TaskList";
+export { default as TaskDetailsModal } from "@/features/tasks/components/TaskDetailsModal";
 export * from "@/features/tasks/store/tasks.store";
 export * from "@/features/tasks/types";
 export * from "@/features/tasks/validation";
